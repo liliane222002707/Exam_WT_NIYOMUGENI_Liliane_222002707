ebc10d51087e84310bc4eb4ea52dbc72f7860a35
liliane222002707 <151009180+liliane222002707@users.noreply.github.com>
2024-05-23 02:10:16 -0700

    Add files via upload

---
+++ liliane/login.php
@@ -0,0 +1,33 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/form.css">
+  <style>
+  </style>
+</head>
+<body class="container">
+  <div class="header">
+    <h2>Login</h2>
+  </div>
+
+  <form method="post" action="login.php" class="login-form">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<input type="text" name="username" placeholder="Username">
+  	</div>
+  	<div class="input-group">
+  		<input type="password" name="password" placeholder="Password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Log in</button>
+  	</div>
+  	<p class="signup-link">
+  		Don't have an account? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
